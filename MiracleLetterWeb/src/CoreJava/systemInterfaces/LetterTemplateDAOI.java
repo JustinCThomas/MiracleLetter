84b8af72b8093af4a36bb1a00f41d4ed8455a84b
Justin Thomas <ThomasCJustin@gmail.com>
2018-05-24 18:30:37 -0400

    remove capitalized folders

--- MiracleLetterWeb/src/CoreJava/systemInterfaces/LetterTemplateDAOI.java
+++
@@ -1,9 +0,0 @@
-package CoreJava.systemInterfaces;
-
-import java.sql.SQLException;
-
-import MiracleLetter.Models.LetterTemplate;
-
-public interface LetterTemplateDAOI {
-	public LetterTemplate getLetterTemplateByName(String name) throws SQLException;
-}
