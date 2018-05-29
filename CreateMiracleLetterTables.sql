26b094918721ac9731ebdf75d557e62b932e3705
Justin Thomas <ThomasCJustin@gmail.com>
2018-05-29 16:50:14 -0400

    Add starting customer data

--- CreateMiracleLetterTables.sql
+++ CreateMiracleLetterTables.sql
@@ -6,6 +6,12 @@ CONSTRAINT customer_pk PRIMARY KEY(customer_id),
 CONSTRAINT password_length_ck CHECK (LENGTH(password) >= 8)
 );
 
+INSERT INTO customer (email_address, password)
+VALUES ('tim@gmail.com', 'fiveYard');
+INSERT INTO customer (email_address, password)
+VALUES ('jim@yahoo.com', 'fourfour');
+
+
 CREATE TABLE Letter_Template(
 letter_template_id NUMERIC GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1) NOT NULL,
 letter_template_name VARCHAR2(255) NOT NULL UNIQUE,
@@ -13,6 +19,13 @@ letter_template_description VARCHAR2(255) NOT NULL,
 letter_template_base_price NUMBER(10,2) NOT NULL,
 CONSTRAINT letter_template_pk PRIMARY KEY(letter_template_id)
 );
+
+INSERT INTO letter_template (letter_template_name, letter_template_description, letter_template_base_price)
+VALUES ('Ocean', 'For those who want to add depth to their letters.', 7.99);
+INSERT INTO letter_template (letter_template_name, letter_template_description, letter_template_base_price)
+VALUES ('Sky', 'Let your creativity soar. The sky''s the limit!', 8.49);
+INSERT INTO letter_template (letter_template_name, letter_template_description, letter_template_base_price)
+VALUES ('Fruit Basket', 'If you need some flavor, this is the letter for you.', 7.49);
 
 CREATE TABLE Envelope(
 envelope_id NUMERIC GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1) NOT NULL,
@@ -22,6 +35,21 @@ envelope_price NUMBER(10,2) NOT NULL,
 CONSTRAINT envelope_pk PRIMARY KEY(envelope_id)
 );
 
+
+INSERT INTO envelope (envelope_type, envelope_color, envelope_price)
+VALUES ('Regular', 'White', 1.29);
+INSERT INTO envelope (envelope_type, envelope_color, envelope_price)
+VALUES ('Window', 'White', 1.49);
+INSERT INTO envelope (envelope_type, envelope_color, envelope_price)
+VALUES ('Regular', 'Blue', 1.49);
+INSERT INTO envelope (envelope_type, envelope_color, envelope_price)
+VALUES ('Window', 'Blue', 1.79);
+INSERT INTO envelope (envelope_type, envelope_color, envelope_price)
+VALUES ('Regular', 'Red', 1.49);
+INSERT INTO envelope (envelope_type, envelope_color, envelope_price)
+VALUES ('Window', 'Red', 1.79);
+
+
 CREATE TABLE Order_Table(
 order_id NUMERIC GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1) NOT NULL,
 customer_id INTEGER NOT NULL,
@@ -30,6 +58,14 @@ date_of_purchase DATE DEFAULT SYSDATE NOT NULL,
 CONSTRAINT order_id PRIMARY KEY(order_id),
 CONSTRAINT customer_id_fk FOREIGN KEY(customer_id) REFERENCES Customer(customer_id)
 );
+
+
+INSERT INTO order_table (customer_id, order_price)
+VALUES (1, 8.78);
+INSERT INTO order_table (customer_id, order_price)
+VALUES (2, 19.06);
+
+
 
 CREATE TABLE Order_Item_Table(
 order_item_id NUMERIC GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1) NOT NULL,
@@ -43,5 +79,12 @@ CONSTRAINT order_id_fk FOREIGN KEY(order_id) REFERENCES Order_Table(order_id) ON
 CONSTRAINT letter_template_id_fk FOREIGN KEY(letter_template_id) REFERENCES Letter_template(letter_template_id),
 CONSTRAINT envelope_id_fk FOREIGN KEY(envelope_id) REFERENCES Envelope(envelope_id)
 );
+
+INSERT INTO order_item_table (order_id, letter_template_id, envelope_id, price_of_item, address_of_recipient)
+VALUES (1, 1, 3, 8.78, '2200 Canal Road');
+INSERT INTO order_item_table (order_id, letter_template_id, envelope_id, price_of_item, address_of_recipient)
+VALUES (2, 1, 4, 9.78, '1400 Windmill Path');
+INSERT INTO order_item_table (order_id, letter_template_id, envelope_id, price_of_item, address_of_recipient)
+VALUES (2, 3, 6, 9.28, '1620 Fulton Street');
 
 COMMIT;
