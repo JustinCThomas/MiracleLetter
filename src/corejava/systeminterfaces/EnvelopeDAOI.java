f4f2de45c9a1814d30a871bcff9cc4c5464112e1
Justin Thomas <ThomasCJustin@gmail.com>
2018-06-20 18:34:53 -0400

    Remove Extra Files/Folders/

--- src/corejava/systeminterfaces/EnvelopeDAOI.java
+++
@@ -1,9 +0,0 @@
-package corejava.systeminterfaces;
-
-import java.sql.SQLException;
-
-import miracleletter.models.Envelope;
-
-public interface EnvelopeDAOI {
-	public Envelope getEnvelopeById(int id) throws SQLException;
-}
--- src/corejava/systeminterfaces/LetterTemplateDAOI.java
+++
@@ -1,9 +0,0 @@
-package corejava.systeminterfaces;
-
-import java.sql.SQLException;
-
-import miracleletter.models.LetterTemplate;
-
-public interface LetterTemplateDAOI {
-	public LetterTemplate getLetterTemplateByName(String name) throws SQLException;
-}
